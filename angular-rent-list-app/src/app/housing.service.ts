596107b117f1b7aa4a8a5a2f0241398c480609d8
Stryfura <shilunett@gmail.com>
2025-05-15 00:06:21 +0200

    Added form to apply for housing

--- angular-rent-list-app/src/app/housing.service.ts
+++ angular-rent-list-app/src/app/housing.service.ts
@@ -116,6 +116,11 @@ export class HousingService {
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find((housingLocation) => housingLocation.id === id);
   }
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`,
+    );
+  }
 
   constructor() { }
 }
