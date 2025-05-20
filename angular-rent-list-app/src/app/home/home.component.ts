4ff3284103f4fe4b965a2cb2db685f9bc626913e
Stryfura <shilunett@gmail.com>
2025-05-20 14:45:26 +0200

    Added json server to retrieve data with a promise using fetch

--- angular-rent-list-app/src/app/home/home.component.ts
+++ angular-rent-list-app/src/app/home/home.component.ts
@@ -32,15 +32,16 @@ export class HomeComponent {
   filteredLocationList: HousingLocation[] = [];
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
-    this.filteredLocationList = this.housingLocationList;
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
   }
   filterResults(text: string) {
     if (!text) {
       this.filteredLocationList = this.housingLocationList;
       return;
     }
-
     this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
       housingLocation?.city.toLowerCase().includes(text.toLowerCase())
     );
